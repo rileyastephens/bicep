59fa369ab91446200d00547c4c05f3547d31c4b3
rileyastephens <rileste@cdw.com>
2021-03-23 14:16:49 -0500

    Created rg module and workflow file

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
+# Azure Bicep
+
+## Welcome!
+If you're new to Azure Bicep, welcome to the club!
+
+## Install
+
+### Azure CLI
 AzCLI version 2.20.0 or greater
+`az upgrade`
+
+Install bicep:
+`az bicep install`
---
+++ local-testing.ps1
@@ -0,0 +1,11 @@
+# Run a az bicep build on all Bicep modules
+Get-ChildItem -Path ./bicep-modules/**/* -Filter *.bicep | ForEach-Object { az bicep build --files $_.FullName }
+
+# Make sure the latest version of ARM TTK exists in the /arm-ttk folder
+git clone https://github.com/Azure/arm-ttk.git
+
+# Import the TTK Module
+Import-Module .\arm-ttk\arm-ttk\arm-ttk.psd1
+
+# Test the Bicep modules
+Test-AzTemplate -TemplatePath .\bicep-modules\resource-group\create-rg.json
